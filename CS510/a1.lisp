b310e63b24f0bb2b9aea0e0bfab095dd00bb12f9
Dustin Ingram <dustin.ingram@gmail.com>
2012-10-10 21:13:17 -0400

    Finished BFS

--- CS510/a1.lisp
+++ CS510/a1.lisp
@@ -13,3 +13,39 @@
     "Returns successor states of the puzzle"
     (mapcar #'(lambda (x) (move puzzle x))
     (valid-moves puzzle)))
+
+(defun apply-moves (puzzle moves)
+    (if (null moves) puzzle
+        (apply-moves (move puzzle (car moves)) (cdr moves))))
+
+
+(defun solve-8puzzle (cmd puzzle)
+    (print-puzzle puzzle)
+    (case cmd
+        ((BFS) (bfs puzzle))
+        ((DFS) (print "dfs"))
+        ((A*) (print "a*"))))
+
+(defun bfs (puzzle)
+    (let ((visited '())
+          (queue (list (list puzzle '()))))
+        (loop while (not (null queue)) do
+            (cond 
+                ((solved? (caar queue)) 
+                    (print "solved")
+                    (return (cadar queue))
+                )
+                (T 
+                    (mapcar #'(lambda (x) 
+                    (if (not (member (move (caar queue) x) visited))
+                    
+                    
+                        (setf queue (append queue (list (list (move (caar queue) x) (append (cadar queue) (list x))))))
+                        )
+                    )
+                    (valid-moves (caar queue)))
+                    (setf queue (cdr queue))
+                    (setf visited (append visited (list (caar queue))))
+                )
+            ) 
+            )))
