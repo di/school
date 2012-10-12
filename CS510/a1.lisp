df9976201224a560384ac0345ec41385162dc92e
Dustin Ingram <dustin.ingram@gmail.com>
2012-10-12 19:13:43 -0400

    Finished programming, started written

--- CS510/a1.lisp
+++ CS510/a1.lisp
@@ -1,51 +1,44 @@
 ;; 8-puzzle
 ;; Dustin Ingram
-
-(defun solved-puzzle ()
-    "Returns a solved puzzle"
-    #2a((1 2 3)(4 5 6)(7 8 9)))
+(load "8puzzle.lisp")
 
 (defun solved? (puzzle)
-    "Returns if the list is solved"
-    (equalp puzzle (solved-puzzle)))
-
-(defun successors (puzzle)
-    "Returns successor states of the puzzle"
-    (mapcar #'(lambda (x) (move puzzle x))
-    (valid-moves puzzle)))
-
-(defun apply-moves (puzzle moves)
-    (if (null moves) puzzle
-        (apply-moves (move puzzle (car moves)) (cdr moves))))
-
+    (equalp puzzle #2a((1 2 3)(4 5 6)(7 8 9))))
 
 (defun solve-8puzzle (cmd puzzle)
-    (print-puzzle puzzle)
-    (case cmd
-        ((BFS) (bfs puzzle))
-        ((DFS) (print "dfs"))
-        ((A*) (print "a*"))))
+  (let ((visited '()) (queue (list (list puzzle '()))))
+    (loop while (not (null queue)) do
+      (let* ((cur (pop queue)) (board (car cur)) (moves (cadr cur)))
+        (push board visited)
+        (if (solved? board) (return moves)
+          (mapcar #'(lambda (x) 
+            (if (not (member (move board x) visited :test 'equalp))
+              (insert (list (move board x) (append moves (list x))) queue cmd)))
+            (valid-moves board)))))))
+ 
+(defmacro insert (e l cmd)
+  `(case cmd
+    ((BFS) (setf ,l (append ,l (list ,e))))
+    ((DFS) (push ,e ,l))
+    ((A*) (setf ,l (append ,l (list (append ,e (list (manhattan ,e))))))
+          (sort ,l #'(lambda (a b) (<= (third a) (third b)))))))
 
-(defun bfs (puzzle)
-    (let ((visited '())
-          (queue (list (list puzzle '()))))
-        (loop while (not (null queue)) do
-            (cond 
-                ((solved? (caar queue)) 
-                    (print "solved")
-                    (return (cadar queue))
-                )
-                (T 
-                    (mapcar #'(lambda (x) 
-                    (if (not (member (move (caar queue) x) visited))
-                    
-                    
-                        (setf queue (append queue (list (list (move (caar queue) x) (append (cadar queue) (list x))))))
-                        )
-                    )
-                    (valid-moves (caar queue)))
-                    (setf queue (cdr queue))
-                    (setf visited (append visited (list (caar queue))))
-                )
-            ) 
-            )))
+(defun manhattan (e)
+  (let ((r (length (cadr e))) (board (car e)))
+    (loop for row from 0 to 2 do
+      (loop for col from 0 to 2 do
+        (let ((i (value board row col)))
+          (setf r (+ r
+            (abs (- row (floor (/ (- i 1) 3))))
+            (abs (- col (mod (+ 2 i) 3))))))))
+    r))
+
+(defun timeit (n)
+  (let ((puzzles '()))
+    (loop while (< (length puzzles) n) do
+      (push (random-puzzle) puzzles))
+    (loop for cmd in '(BFS DFS A*) do
+      (print cmd)
+      (time 
+        (loop for puzzle in puzzles do
+          (solve-8puzzle cmd puzzle))))))
